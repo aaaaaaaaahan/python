4db982a0d11362a24d52a9e9244d60cb2b9e5788
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-09-04 11:51:27 +0800

    Update CICMDRPT.py

--- CICMDRPT.py
+++ CICMDRPT.py
@@ -1,1 +1,12 @@
+# Apply padding logic (like IF … THEN in SAS)
+df = df.with_columns([
+    pl.when(pl.col("OPENMM").cast(pl.Int32) < 10)
+      .then(pl.lit("0") + pl.col("OPENMM").str.slice(-1, 1))
+      .otherwise(pl.col("OPENMM"))
+      .alias("OPENMM"),
 
+    pl.when(pl.col("CLSEMM").cast(pl.Int32) < 10)
+      .then(pl.lit("0") + pl.col("CLSEMM").str.slice(-1, 1))
+      .otherwise(pl.col("CLSEMM"))
+      .alias("CLSEMM"),
+])
