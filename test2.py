ec457218fc2132569ac8e8bab08ac9a2f5d4fe79
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-09-08 17:26:21 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,15 +1,6 @@
-taxid_oldic = taxid_oldic.with_columns([
-    pl.when((pl.col("C") == 1) & (pl.col("F") == 1))
-      .then("B")
-      .when((pl.col("C") == 1) & pl.col("F").is_null())
-      .then("N")
-      .when(pl.col("C").is_null() & (pl.col("F") == 1))
-      .then("O")
-      .otherwise("X")
-      .alias("MATCHID"),
-
-    pl.when((pl.col("C") == 1) | (pl.col("F") == 1))
-      .then("Y")
-      .otherwise("N")
-      .alias("RHOLD_IND")
-])
+  DATA TAXID_NEWIC;                                                    
+      MERGE TAXID(IN=A) RHOLD(IN=B RENAME=(ALIAS=NEWIC1)); BY NEWIC1;  
+      IF A;                                                            
+      IF A AND B THEN C = 1;                                           
+  RUN;                                                                 
+PROC SORT DATA=TAXID_NEWIC; BY OLDIC; RUN;                             
