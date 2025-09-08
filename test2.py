a51ff0a00f84a3442a125c942fd2a037f3563b59
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-09-08 16:40:25 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,15 @@
+taxid_oldic = taxid_oldic.with_columns([
+    pl.when((pl.col("C") == 1) & (pl.col("F") == 1))
+      .then("B")
+      .when((pl.col("C") == 1) & pl.col("F").is_null())
+      .then("N")
+      .when(pl.col("C").is_null() & (pl.col("F") == 1))
+      .then("O")
+      .otherwise("X")
+      .alias("MATCHID"),
 
+    pl.when((pl.col("C") == 1) | (pl.col("F") == 1))
+      .then("Y")
+      .otherwise("N")
+      .alias("RHOLD_IND")
+])
