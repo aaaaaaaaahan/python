f96a7dd220f1dfc9d10d5e2aefc16cbcd5a8550d
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-09-10 15:20:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,92 +1,133 @@
-import duckdb
-import pyarrow as pa
-import pyarrow.parquet as pq
-import pyarrow.csv as pc
-from reader import load_input  # your loader
+import polars as pl
+from reader import load_input
 
-#--------------------------------#
-# READ PARQUET FILES WITH DUCKDB #
-#--------------------------------#
-con = duckdb.connect()
+# -----------------------------
+# Part 0: Read Parquet files
+# -----------------------------
+oldic = load_input("CCRIS_OLDIC_GDG")    
+newic = load_input("CCRIS_ALIAS_GDG")      
+rhold = load_input("RHOLD_FULL_LIST")  
 
-aown_raw   = load_input("ADDRAOWN_FB")
-dpaddr_raw = load_input("DP_CUST_DAILY_ADDRACC")
+# -----------------------------
+# Part 1: OLDIC processing
+# -----------------------------
+oldic = oldic.select([
+    pl.col("CUSTNO").cast(pl.Int64).cast(pl.Utf8).str.zfill(11),
+    pl.col("CODE_OLD").cast(pl.Utf8),
+    pl.col("INDORG").cast(pl.Utf8),
+    pl.col("OLDIC").cast(pl.Utf8),
+    pl.col("CUSTBRCH").cast(pl.Int64).cast(pl.Utf8).str.zfill(5)
+])
+oldic = oldic.sort("CUSTNO")
+print("OLD IC:\n", oldic.head(5))
 
-# Register into DuckDB
-con.register("aown_raw", aown_raw)
-con.register("dpaddr_raw", dpaddr_raw)
+# -----------------------------
+# Part 2: NEWIC processing
+# -----------------------------
+newic = newic.select([
+    pl.col("CUSTNO").cast(pl.Int64).cast(pl.Utf8).str.zfill(11),
+    pl.col("CODE_NEW").cast(pl.Utf8),
+    pl.col("NEWIC").cast(pl.Utf8),
+    pl.col("KEYFIELD1").cast(pl.Utf8),
+    pl.col("KEYFIELD2").cast(pl.Utf8)
+])
+newic = newic.with_columns(
+    pl.col("NEWIC").str.slice(3, 20).alias("NEWIC1")  # SUBSTR(NEWIC,4,20)
+)
+newic = newic.sort("CUSTNO")
+print("NEW IC:\n", newic.head(5))
 
-#--------------------------------#
-# Part 1 - PROCESS AOWNFILE      #
-#--------------------------------#
-con.execute("""
-    CREATE OR REPLACE TEMP VIEW aown AS
-    SELECT DISTINCT
-        CASE 
-            WHEN LEFT(ACCTNO,1) = '0' THEN SUBSTRING(ACCTNO,2)
-            ELSE ACCTNO
-        END AS ACCTNO,
-        O_APPL_CODE,
-        NA_LINE_TYP1, NA_LINE_TYP2, NA_LINE_TYP3, NA_LINE_TYP4,
-        NA_LINE_TYP5, NA_LINE_TYP6, NA_LINE_TYP7, NA_LINE_TYP8
-    FROM aown_raw
-    WHERE O_APPL_CODE = 'DP'
-      AND ACCTNO > '10000000000'
-""")
+# -----------------------------
+# Part 3: RHOLD processing
+# -----------------------------
+rhold_alias1 = rhold.select(pl.col("ID1").alias("ALIAS")).filter(pl.col("ALIAS") != "")
+rhold_alias2 = rhold.select(pl.col("ID2").alias("ALIAS")).filter(pl.col("ALIAS") != "")
+rhold_all = pl.concat([rhold_alias1, rhold_alias2]).unique(subset="ALIAS").sort("ALIAS")
+print("RHOLD:\n", rhold_all.head(5))
 
-print("AOWN SAMPLE")
-print(con.execute("SELECT * FROM aown LIMIT 5").fetchdf())
+# -----------------------------
+# Part 4: TAXID merge OLDIC + NEWIC
+# -----------------------------
+taxid = oldic.join(newic, on="CUSTNO", how="left")
+taxid = taxid.with_columns(
+    pl.when(pl.col("INDORG") == "O")
+      .then(pl.col("NEWIC"))
+      .otherwise(None)
+      .alias("BUSREG")
+)
+taxid = taxid.sort("NEWIC1")
+print("TAXID FILE:\n", taxid.head(5))
 
-#--------------------------------#
-# Part 2 - PROCESS DEPOSIT FILE  #
-#--------------------------------#
-con.execute("""
-    CREATE OR REPLACE TEMP VIEW dpaddr AS
-    SELECT DISTINCT
-        CAST(ACCTNO AS VARCHAR) AS ACCTNO,
-        ADD_NAME_1, ADD_NAME_2, ADD_NAME_3, ADD_NAME_4,
-        ADD_NAME_5, ADD_NAME_6, ADD_NAME_7, ADD_NAME_8
-    FROM dpaddr_raw
-    WHERE ACCTNO > '10000000000'
-""")
+# -----------------------------
+# Part 5: TAXID_NEWIC merge with RHOLD (NEWIC1)
+# -----------------------------
+rhold_newic = rhold_all.rename({"ALIAS": "NEWIC1"})
+taxid_newic = taxid.join(rhold_newic, on="NEWIC1", how="left")
+taxid_newic = taxid_newic.with_columns(
+    pl.when(pl.col("NEWIC1").is_not_null())
+      .then(1)
+      .otherwise(0)
+      .alias("C")
+)
+taxid_newic = taxid_newic.sort("OLDIC")
 
-print("DPADDR SAMPLE")
-print(con.execute("SELECT * FROM dpaddr LIMIT 5").fetchdf())
+print("taxid_newic:")
+print(taxid_newic.head(5))
 
-#--------------------------------#
-# Part 3 - MERGE ON ACCTNO       #
-#--------------------------------#
-con.execute("""
-    CREATE OR REPLACE TEMP VIEW merged AS
-    SELECT 
-        a.O_APPL_CODE,
-        a.ACCTNO,
-        a.NA_LINE_TYP1, d.ADD_NAME_1,
-        a.NA_LINE_TYP2, d.ADD_NAME_2,
-        a.NA_LINE_TYP3, d.ADD_NAME_3,
-        a.NA_LINE_TYP4, d.ADD_NAME_4,
-        a.NA_LINE_TYP5, d.ADD_NAME_5,
-        a.NA_LINE_TYP6, d.ADD_NAME_6,
-        a.NA_LINE_TYP7, d.ADD_NAME_7,
-        a.NA_LINE_TYP8, d.ADD_NAME_8
-    FROM aown a
-    INNER JOIN dpaddr d ON d.ACCTNO = a.ACCTNO
-    ORDER BY a.ACCTNO
-""")
+# -----------------------------
+# Part 6: TAXID_OLDIC merge with RHOLD (OLDIC)
+# -----------------------------
+rhold_oldic = rhold_all.rename({"ALIAS": "OLDIC"})
+taxid_oldic = taxid_newic.join(rhold_oldic, on="OLDIC", how="left")
+taxid_oldic = taxid_oldic.with_columns(
+    pl.when(pl.col("OLDIC").is_not_null())
+      .then(1)
+      .otherwise(0)
+      .alias("F")
+)
+taxid_oldic = taxid_oldic.sort("CUSTNO")
 
-print("MERGED SAMPLE")
-print(con.execute("SELECT * FROM merged LIMIT 5").fetchdf())
+print("taxid_oldic:")
+print(taxid_oldic.head(5))
 
-#--------------------------------#
-# Part 4 - OUTPUT                #
-#--------------------------------#
-out_arrow: pa.Table = con.execute("SELECT * FROM merged").arrow()
+# -----------------------------
+# Part 7: OUT dataset creation
+# -----------------------------
+def match_type(row):
+    if row["C"] == 1 and row["F"] == 1:
+        return "B", "Y"
+    elif row["C"] == 1 and row["F"] is None:
+        return "N", "Y"
+    elif row["C"] is None and row["F"] == 1:
+        return "O", "Y"
+    else:
+        return "X", "N"
 
-pq.write_table(out_arrow, "output/cis_internal/DAILY_ADDRACC.parquet")
+taxid_oldic = taxid_oldic.with_columns([
+    pl.when((pl.col("C") == 1) & (pl.col("F") == 1))
+      .then(pl.lit("B"))
+      .when((pl.col("C") == 1) & (pl.col("F") == 0))
+      .then(pl.lit("N"))
+      .when((pl.col("C") == 0) & (pl.col("F") == 1))
+      .then(pl.lit("O"))
+      .otherwise(pl.lit("X"))
+      .alias("MATCHID"),
 
-with open("output/cis_internal/DAILY_ADDRACC.csv", "wb") as f:
-    pc.write_csv(out_arrow, f)
+    pl.when((pl.col("C") == 1) | (pl.col("F") == 1))
+      .then(pl.lit("Y"))
+      .otherwise(pl.lit("N"))
+      .alias("RHOLD_IND")
+])
 
-print("OUT FILE SAMPLE")
-print(out_arrow.to_pandas().head(5))
+# -----------------------------
+# Part 8: Write output
+# -----------------------------
+taxid_oldic.select([
+    "CUSTNO", "OLDIC", "NEWIC", "BUSREG", "CUSTBRCH", "RHOLD_IND", "MATCHID"
+]).write_parquet("cis_internal/output/CCRIS_TAXID_GDG.parquet")
+taxid_oldic.select([
+    "CUSTNO", "OLDIC", "NEWIC", "BUSREG", "CUSTBRCH", "RHOLD_IND", "MATCHID"
+]).write_csv("cis_internal/output/CCRIS_TAXID_GDG.csv")
 
+print("Final Output:")
+print(taxid_oldic.head(5))
