e2c4b74deb2e4797d0435e42b3c7589199d647f2
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-09-29 11:00:02 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,11 +1,8 @@
-          lrecl = 100
-
-
-INPUT @01   BANKNO            S370F3.  
-      @05   DEMOCATEGORY      $EBCDIC5.
-      @10   DEMOCODE          $EBCDIC3.  
-      @10   DEMOCODX          $EBCDIC2.  
-      @15   EFFDATE           S370F7.  
-      @25   ENTRYDATE         S370F13. 
-      @40   RLENDESC          $EBCDIC20.
-      ;
+con.execute(f"""
+    CREATE OR REPLACE TABLE RLENCA AS
+    SELECT
+        RLENTYPE,
+        CAST(RLENCODE AS INTEGER) AS RLENCODE,
+        RLENDESC AS RELATIONDESC
+    FROM parquet_scan('{RLENCAFILE}')
+""")
