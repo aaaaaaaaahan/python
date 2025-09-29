7a2169f723333dbc647a2375bbc4cbde9c2204e5
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-09-29 17:00:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,15 @@
 # ================================================================
-# Deposit Trial Balance (split MYR / non-MYR)
+# Merge DPOTH with FOREX
 # ================================================================
-con.execute(f"""
-    CREATE OR REPLACE TABLE DP_ALL AS
-    SELECT
-        BANKNO,
-        REPTNO,
-        FMTCODE,
-        ACCTBRCH1 AS BRANCHNO,
-        ACCTNO,
-        ACCTX,
-        CLSEDATE,
-        OPENDATE,
-        HOLDAMT1/100.0 AS HOLDAMT,
-        LEDGERBAL1/100.0 AS LEDGERBAL,
-        ODLIMIT,
-        CURRCODE,
-        OPENIND,
-        DORMIND,
-        COSTCTR,
-        POSTIND,
-        CASE 
-            WHEN COSTCTR > 3000 AND COSTCTR < 3999 THEN 'I'
-            ELSE 'C'
-        END AS BANKINDC,
-        CASE 
-            WHEN CURRCODE <> 'MYR' THEN LEDGERBAL1/100.0
-            ELSE 0
-        END AS FOREXAMT,
-        CASE 
-            WHEN OPENIND = ''  THEN 'ACTIVE'
-            WHEN OPENIND IN ('B','C','P') THEN 'CLOSED'
-            WHEN OPENIND = 'Z' THEN 'ZERO BALANCE'
-            ELSE ''
-        END AS ACCTSTATUS,
-        substr(OPENDATE,4,2) AS OPENDD,
-        substr(OPENDATE,2,2) AS OPENMM,
-        substr(OPENDATE,6,4) AS OPENYY,
-        substr(CLSEDATE,4,2) AS CLSEDD,
-        substr(CLSEDATE,2,2) AS CLSEMM,
-        substr(CLSEDATE,6,4) AS CLSEYY
-    FROM '{host_parquet_path("DPTRBLGS.parquet")}'
-    WHERE REPTNO = 1001
-      AND FMTCODE IN (1,10,22,19,20,21)
-      AND ACCTNO > 1000000000 AND ACCTNO < 1999999999
+con.execute("""
+    CREATE OR REPLACE TABLE FOREXMRG AS
+    SELECT a.*,
+           b.FOREXRATE,
+           ROUND(((a.FOREXAMT * b.FOREXRATE)/0.01)::INT * 0.01, 2) AS LEDGERBAL
+    FROM DPOTH a
+    LEFT JOIN FOREX b
+    ON a.CURRCODE = b.CURRCODE
 """)
+
+print("FOREXMRG (first 5 rows):")
+print(con.execute("SELECT * FROM FOREXMRG LIMIT 5").fetchdf())
