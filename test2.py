abb82b00d17f9972f4afee7d49313e97a3ebe266
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-09-30 11:52:42 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,11 +1,8 @@
-CREATE OR REPLACE TABLE FOREXMRG AS
-SELECT 
-    a.*,
-    b.FOREXRATE,
-    ROUND(
-        CAST((a.FOREXAMT * COALESCE(CAST(b.FOREXRATE AS DOUBLE), 0.0)) / 0.01 AS INTEGER) * 0.01,
-        2
-    ) AS LEDGERBAL
-FROM DPOTH a
-LEFT JOIN FOREX b
-ON a.CURRCODE = b.CURRCODE
+Error in sitecustomize; set PYTHONVERBOSE for traceback:
+TypeError: expected str, bytes or os.PathLike object, not NoneType
+Traceback (most recent call last):
+  File "/pythonITD/cis_dev/jobs/cis_internal/CIS_PY_CCRNMX3B.py", line 91, in <module>
+    con.execute(f"""
+duckdb.duckdb.ConversionException: Conversion Error: Could not convert string 'I' to INT64 when casting from source column SECPHONE
+
+LINE 16:     FROM name_clean n
