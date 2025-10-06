37341483cda0150194c31ed826815d6c5d21e1fc
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-06 16:28:14 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -5,83 +5,44 @@ import datetime
 batch_date = (datetime.date.today() - datetime.timedelta(days=1))
 year, month, day = batch_date.year, batch_date.month, batch_date.day
 
-# ================================
+# ==================================
 # CONNECT TO DUCKDB
-# ================================
+# ==================================
 con = duckdb.connect()
 
-# ================================
-# STEP 1 - Load SIGNATOR file
-# ================================
+# ====================================
+# STEP 1 - Load and filter CUST File
+# ====================================
 con.execute(f"""
-    CREATE OR REPLACE TABLE SIGNATORY AS
-    SELECT 
-        BANKNO,
-        ACCTNO,
-        SEQNO,
-        REPLACE(NAME, '\\\\', '\\\\\\\\') AS NAME,  -- escape backslash
-        ID,
-        SIGNATORY,
-        MANDATEE,
-        NOMINEE,
-        STATUS,
-        BRANCHNO,
-        BRANCHX
-    FROM '{host_parquet_path("UNLOAD_CISIGNAT_FB.parquet")}'
+    CREATE OR REPLACE TABLE CIS AS
+    SELECT DISTINCT ON (CUSTNO)
+        ALIASKEY,
+        ALIAS,   
+        CUSTNAME,
+        CUSTNO,  
+        LPAD(CAST(CAST(CUSTBRCH AS INTEGER) AS VARCHAR), 3, '0') AS CUSTBRCH
+    FROM read_parquet('CIS_CUST_DAILY/year=2025/month=9/day=29/data_0.parquet')
+    WHERE CUSTNAME <> ''
+      AND ALIASKEY = 'IC'
+      AND INDORG = 'I'
+      AND CITIZENSHIP = 'MY'
+      AND RACE = 'O'
+    ORDER BY CUSTNO
 """)
 
-# ================================
-# STEP 2 - Sort by ACCTNO + SEQNO
-# ================================
-con.execute("""
-    CREATE OR REPLACE TABLE SIGNATORY_SORTED AS
-    SELECT * 
-    FROM SIGNATORY
-    ORDER BY ACCTNO, SEQNO
-""")
-
-# Optional: Preview like PROC PRINT OBS=10
-preview = con.execute("SELECT * FROM SIGNATORY_SORTED LIMIT 10").fetchdf()
-print("Preview of SIGNATORY:")
-print(preview)
-
-# ================================
-# STEP 3 - Format Output
-# ================================
-# Rebuild SAS PUT formatting into a CSV-style export
-con.execute("""
-    CREATE OR REPLACE TABLE TEMPOUT AS
-    SELECT 
-        '"' || BANKNO      || '","'
-            || ACCTNO      || '","'
-            || SEQNO       || '","'
-            || NAME        || '","'
-            || ID          || '","'
-            || SIGNATORY   || '","'
-            || MANDATEE    || '","'
-            || NOMINEE     || '","'
-            || STATUS      || '","'
-            || LPAD(CAST(BRANCHNO AS VARCHAR), 5, '0') || '","'
-            || BRANCHX     || '","'
-            || {year} AS year     || '","'
-            || {month} AS month     || '","'
-            || {day} AS day     
-    FROM SIGNATORY_SORTED
-""")
-
-# Fetch as Arrow table
-#arrow_tbl = con.execute("SELECT RECORD_LINE FROM TEMPOUT").fetch_arrow_table()
-
-# ================================
-# STEP 4 - Save Output
-# ================================
+# ==================================
+# STEP 4 - PRINT SAMPLE (OBS=5)
+# ==================================
 out1 = """
     SELECT *
-    FROM TEMPOUT
+           ,{year} AS year
+           ,{month} AS month
+           ,{day} AS day
+    FROM CIS
 """.format(year=year,month=month,day=day)
 
 queries = {
-    "SNGLVIEW_SIGN"            : out1
+    "CIS_RACE"            : out1
 }
 
 for name, query in queries.items():
