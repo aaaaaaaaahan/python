783b6f3a9b9abc10179d834ab1a8ddc056230dc8
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-08 09:38:13 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,49 +1,13 @@
-import duckdb
-from CIS_PY_READER_JKH import host_parquet_path,parquet_output_path,csv_output_path, get_hive_parquet
-import datetime
-
-batch_date = (datetime.date.today() - datetime.timedelta(days=1))
-year, month, day = batch_date.year, batch_date.month, batch_date.day
-
-# ==================================
-# CONNECT TO DUCKDB
-# ==================================
-con = duckdb.connect()
-CIS, year, month, day = get_hive_parquet('AMLHRC_EXTRACT_MASSCLS')
-
-# ====================================
-# STEP 1 - Load and filter CUST File
-# ====================================
-con.execute(f"""
-    CREATE OR REPLACE TABLE CIS AS
-    SELECT *
-    FROM read_parquet({CIS})
-""")
-
-# ==================================
-# STEP 4 - PRINT SAMPLE (OBS=5)
-# ==================================
-out1 = """
-    SELECT *
-    FROM CIS
+account_filter = """
+    ACCTCODE IN ('LN','DP')
+    AND (
+        (CAST(ACCTNO AS BIGINT) BETWEEN 1000000000 AND 1999999999) OR
+        (CAST(ACCTNO AS BIGINT) BETWEEN 2000000000 AND 2999999999) OR
+        (CAST(ACCTNO AS BIGINT) BETWEEN 3000000000 AND 3999999999) OR
+        (CAST(ACCTNO AS BIGINT) BETWEEN 4000000000 AND 4999999999) OR
+        (CAST(ACCTNO AS BIGINT) BETWEEN 5000000000 AND 5999999999) OR
+        (CAST(ACCTNO AS BIGINT) BETWEEN 6000000000 AND 6999999999) OR
+        (CAST(ACCTNO AS BIGINT) BETWEEN 7000000000 AND 7999999999) OR
+        (CAST(ACCTNO AS BIGINT) BETWEEN 8000000000 AND 8999999999)
+    )
 """
-
-queries = {
-    "test_jkh"            : out1
-}
-
-for name, query in queries.items():
-    parquet_path = parquet_output_path(name)
-    csv_path = csv_output_path(name)
-
-    con.execute(f"""
-    COPY ({query})
-    TO '{parquet_path}'
-    (FORMAT PARQUET, PARTITION_BY (year, month, day), OVERWRITE_OR_IGNORE true);  
-     """)
-    
-    con.execute(f"""
-    COPY ({query})
-    TO '{csv_path}'
-    (FORMAT CSV, HEADER, DELIMITER ',', OVERWRITE_OR_IGNORE true);  
-     """)
