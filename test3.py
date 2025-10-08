6e18c168265ba09d60e94cfc8276f590b1840641
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-08 15:58:07 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,23 +1,3 @@
-duckdb.duckdb.BinderException: Binder Error: No function matches the given name and argument types '*(VARCHAR, INTEGER_LITERAL)'. You might need to add explicit type casts.
-        Candidate functions:
-        *(TINYINT, TINYINT) -> TINYINT
-        *(SMALLINT, SMALLINT) -> SMALLINT
-        *(INTEGER, INTEGER) -> INTEGER
-        *(BIGINT, BIGINT) -> BIGINT
-        *(HUGEINT, HUGEINT) -> HUGEINT
-        *(FLOAT, FLOAT) -> FLOAT
-        *(DOUBLE, DOUBLE) -> DOUBLE
-        *(DECIMAL, DECIMAL) -> DECIMAL
-        *(UTINYINT, UTINYINT) -> UTINYINT
-        *(USMALLINT, USMALLINT) -> USMALLINT
-        *(UINTEGER, UINTEGER) -> UINTEGER
-        *(UBIGINT, UBIGINT) -> UBIGINT
-        *(UHUGEINT, UHUGEINT) -> UHUGEINT
-        *(INTERVAL, DOUBLE) -> INTERVAL
-        *(DOUBLE, INTERVAL) -> INTERVAL
-        *(BIGINT, INTERVAL) -> INTERVAL
-        *(INTERVAL, BIGINT) -> INTERVAL
-
-
-LINE 10:         CASE WHEN SIGN1 = '-' THEN OUTSTNDAMT * -1
-                                                       ^
+ File "/pythonITD/cis_dev/jobs/cis_internal/CIS_PY_CCRCCRL1 copy.py", line 40, in <module>
+    con.execute("""
+duckdb.duckdb.InvalidInputException: Invalid Input Error: Parameter argument/count mismatch, identifiers of the excess parameters: 1
