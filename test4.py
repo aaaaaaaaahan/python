ca07a2fa9a1a17b1b281fb1d6dbff81ff8fc645a
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-08 16:41:16 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,1 +1,50 @@
-printf('%03d', CAST(RLENCODE AS INTEGER)) AS RLENCD
+con.execute("""
+    CREATE TABLE cis_newic AS
+    SELECT
+        ALIAS,
+        TAXID,
+        ALIASKEY,
+        ACCTNOC,
+        CUSTNO,
+        CUSTNAME,
+        PRIMSEC,
+        RLENTYPE,
+        RLENDESC,
+        JOINTACC,
+        ACCTCODE,
+        RLENCODE,
+        RLENCD,
+        'NEWIC' AS TYPE,
+        ALIASKEY || ALIAS AS CUSTID
+    FROM cis
+    WHERE ALIAS <> ''
+""")
+
+con.execute("""
+    CREATE TABLE cis_oldic AS
+    SELECT
+        ALIAS,
+        TAXID,
+        ALIASKEY,
+        ACCTNOC,
+        CUSTNO,
+        CUSTNAME,
+        PRIMSEC,
+        RLENTYPE,
+        RLENDESC,
+        JOINTACC,
+        ACCTCODE,
+        RLENCODE,
+        RLENCD,
+        'OLDIC' AS TYPE,
+        'OC' || TAXID AS CUSTID
+    FROM cis
+    WHERE TAXID <> ''
+""")
+
+con.execute("""
+    CREATE TABLE cis_custids AS
+    SELECT * FROM cis_newic
+    UNION ALL
+    SELECT * FROM cis_oldic
+""")
