93935215ce32769dd34251fec1ad742f3eed67ed
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-14 17:37:46 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,7 +1,15 @@
-PAGE : 1REPORT ID   : SDB/SCREEN/FULL                                   PUBLIC BANK BERHAD               PAGE :   92
-PROGRAM ID  : CISDBFRP                                                       REPORT DATE : 14-10-2025
-BRANCH      : ******          SDB FULL DATABASE SCREENING
-                                                  ===========================
-BOX NO    NAME (HIRER S NAME)                     CUSTOMER ID         
-------------------------------------------------------------------------------------------------------------------------
-*****     ************                             ************        
+if first_page:
+    # --- First page, no spacing before ---
+    page_cnt += 1
+    print_header(f, branch, page_cnt, report_date)
+    current_branch = branch
+    line_cnt = 9
+    first_page = False
+
+elif branch != current_branch or line_cnt >= 55:
+    # --- Add spacing after each page before new one starts ---
+    f.write("\n" * 3)
+    page_cnt += 1
+    print_header(f, branch, page_cnt, report_date)
+    current_branch = branch
+    line_cnt = 9
