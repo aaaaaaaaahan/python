b5313107d352562e6eb5f5f76530972f3c76fa12
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-14 17:04:08 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,7 +1,7 @@
 PAGE : 1REPORT ID   : SDB/SCREEN/FULL                                   PUBLIC BANK BERHAD               PAGE :   92
 PROGRAM ID  : CISDBFRP                                                       REPORT DATE : 14-10-2025
-BRANCH      : 00704          SDB FULL DATABASE SCREENING
+BRANCH      : ******          SDB FULL DATABASE SCREENING
                                                   ===========================
 BOX NO    NAME (HIRER S NAME)                     CUSTOMER ID         
 ------------------------------------------------------------------------------------------------------------------------
-20003     MAISARAH MUSTAFA                             960712145602        
+*****     ************                             ************        
