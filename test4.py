796fe0cff1cf4d9884f8f44d70bef82781db160c
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-14 17:03:41 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,4 +1,7 @@
-state = {'page_cnt': 1, 'line_cnt': 0}
-def print_header(branch):
-    state['line_cnt'] = 9
-    f.write(f"PAGE : {state['page_cnt']}")
+PAGE : 1REPORT ID   : SDB/SCREEN/FULL                                   PUBLIC BANK BERHAD               PAGE :   92
+PROGRAM ID  : CISDBFRP                                                       REPORT DATE : 14-10-2025
+BRANCH      : 00704          SDB FULL DATABASE SCREENING
+                                                  ===========================
+BOX NO    NAME (HIRER S NAME)                     CUSTOMER ID         
+------------------------------------------------------------------------------------------------------------------------
+20003     MAISARAH MUSTAFA                             960712145602        
