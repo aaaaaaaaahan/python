1f41a1eb28c93ba9e89929a964e0658abd0f459d
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-15 14:17:51 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,10 +1,5 @@
-import duckdb
-from CIS_PY_READER import get_hive_parquet
-
-# Get previous (-1) and latest (0) parquet paths
-old_path, new_path = get_hive_parquet("CIS.SDB.MATCH.FULL", debug=True)
-
-# Create DuckDB tables
-con = duckdb.connect()
-con.execute(f"CREATE TABLE old AS SELECT * FROM read_parquet('{old_path}')")
-con.execute(f"CREATE TABLE new AS SELECT * FROM read_parquet('{new_path}')")
+ File "/pythonITD/cis_dev/Programmer/jkh/Hive/Read_Hive_Program.py", line 22, in <module>
+    prev_file, latest_file = get_prev_latest_parquet('CIS_SDB_MATCH_FULL')
+  File "/pythonITD/cis_dev/Programmer/jkh/Hive/CIS_PY_READER_Hive_copy.py", line 381, in get_prev_latest_parquet
+    date_val = datetime(y, m, d)
+TypeError: 'module' object is not callable
