bf35b933384258555f117649d2a20b0c71f2951a
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-16 10:41:36 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,1 +1,4 @@
-output_txt = f"{csv_output_path}CIS_SDB_MATCH_NRPT_{report_date}.txt"
+H                                                                                                                                               
+DE123456789012ABC MANUFACTURING SDN BHD                                 NOT1234567890123400000001234567
+DE123456789013XYZ RESOURCES                                            NOT123456789012350000000045600
+F0000002                                                                                                                                       
