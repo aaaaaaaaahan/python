70acd093f5b1ab02749e3ef595f1145c0ac404ba
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-16 18:42:15 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,5 +1,7 @@
-HHEADERREC000 Company PERKESO Full Load File                                  20251015BATCH001                                                                        
-D000000000001Johnathan Lim Wei Han                                                                                 NOT20251015000001       000000012345
-D000000000002Nur Aisyah Bt Abdullah                                                                                NOT20251015000002       000000007890
-D000000000003Michael Tan Kok Wai                                                                                   NOT20251015000003       000000005500
-F0000003   TOTAL EMPLOYEE RECORDS                                                                                  FOOTER20251015          000000000000
+FYI,
+PYARROW is suitable for small dataset below than 10 million raw data.
+DUCKDB is suitable for us to process our dataset. And the performance will better than PYARROW when process with the high value dataset.
+
+Conclusion for the CIAMLHRC:
+In my code got .arrow() this is one of the PYARROW function.
+PYARROW is using the RAM to process the data. So in this case CIAMLHRC is holding the RAM to cause the whole flow become slow performance compare with last week.
