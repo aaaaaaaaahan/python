b281ebe615236d038ec918e61c4f9fc1684df59e
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-16 10:31:40 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,5 +1,1 @@
- File "/pythonITD/cis_dev/Programmer/jkh/Hive/Read_Hive_Program.py", line 22, in <module>
-    prev_file, latest_file = get_prev_latest_parquet('CIS_SDB_MATCH_FULL')
-  File "/pythonITD/cis_dev/Programmer/jkh/Hive/CIS_PY_READER_Hive_copy.py", line 381, in get_prev_latest_parquet
-    date_val = datetime(y, m, d)
-TypeError: 'module' object is not callable
+output_txt = f"{csv_output_path}CIS_SDB_MATCH_NRPT_{report_date}.txt"
