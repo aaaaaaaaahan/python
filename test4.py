fd8bc29c84004f7a63cffecd926ac074c9831da6
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-17 14:54:18 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,17 +1,3 @@
-# 1️⃣ Latest and latest previous
-get_gdg_generations("CIS.SDB.MATCH.FULL", generations=2)
-
-# 2️⃣ Latest + latest previous + 2nd previous
-get_gdg_generations("CIS.SDB.MATCH.FULL", generations=3)
-
-# 3️⃣ All generations (unlimited)
-get_gdg_generations("CIS.SDB.MATCH.FULL", generations='all')
-
-# 4️⃣ Only previous + 2nd previous (skip latest)
-get_gdg_generations("CIS.SDB.MATCH.FULL", generations=[-1, -2])
-
-# 5️⃣ Latest + 8 previous (total 9)
-get_gdg_generations("CIS.SDB.MATCH.FULL", generations=9)
-
-# 6️⃣ Custom pattern
-get_gdg_generations("CIS.SDB.MATCH.FULL", generations=[0, -3, -5])
+File "/pythonITD/cis_dev/Programmer/jkh/Hive/CIS_PY_READER_Hive_copy.py", line 357, in <module>
+    generations: int | List[int] | str = 2,
+TypeError: unsupported operand type(s) for |: 'type' and '_GenericAlias'
