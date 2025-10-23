c2e84bf1619c28c84d966e3d391f7065265c1f16
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-23 17:41:00 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -12,3 +12,15 @@ con.execute("""
         NULL AS LAST_INDICATOR
     LIMIT 0
 """)
+
+con.execute("""
+    CREATE OR REPLACE TABLE emplfull2 (
+        RECORD_TYPE VARCHAR,
+        NEW_EMPL_CODE VARCHAR,
+        OLD_EMPL_CODE VARCHAR,
+        EMPL_NAME VARCHAR,
+        ACR_RECEIPT_NO VARCHAR,
+        ACR_AMOUNT VARCHAR,
+        LAST_INDICATOR VARCHAR
+    )
+""")
