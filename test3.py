4444ee1a8fff783c5a47bd0dcb12a2e400e94ceb
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-23 17:39:00 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,1 +1,14 @@
 duckdb.duckdb.BinderException: Binder Error: table emplfull2 has 1 columns but 7 values were supplied
+
+con.execute("""
+    CREATE OR REPLACE TABLE emplfull2 AS 
+    SELECT 
+        NULL AS RECORD_TYPE,
+        NULL AS NEW_EMPL_CODE,
+        NULL AS OLD_EMPL_CODE,
+        NULL AS EMPL_NAME,
+        NULL AS ACR_RECEIPT_NO,
+        NULL AS ACR_AMOUNT,
+        NULL AS LAST_INDICATOR
+    LIMIT 0
+""")
