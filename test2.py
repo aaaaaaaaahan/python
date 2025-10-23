b1bbf5a7bbb8138b7af1d1f2e03e02fd8a8077fc
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-23 15:15:45 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,5 @@
-ypeError: unsupported operand type(s) for |: 'type' and '_GenericAlias'
+TypeError: unsupported operand type(s) for |: 'type' and '_GenericAlias'
+
+from typing import List, Union
+
+def get_hive_parquet(dataset_name: str, generations: Union[int, List[int], str] = 1, debug: bool = False) -> List[str]:
