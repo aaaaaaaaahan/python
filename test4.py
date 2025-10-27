0e6dcfae57ea60d5edfcf633d187f8fe585e4f30
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-10-27 15:54:32 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,8 +1,3 @@
-ERROR: duckdb.duckdb.ConversionException: Conversion Error: Could not convert string '\0' to INT32 when casting from source column C_IBS_E1_TO_E2
-
-LINE 11:       AND RLENCODE IN (3,11,12,13,14,16,17,18,19,21,22,23,27,28)
-
-program:
 import duckdb
 from CIS_PY_READER import host_parquet_path,parquet_output_path,csv_output_path
 import datetime
@@ -42,8 +37,8 @@ con.execute(f"""
         U_IBS_APPL_NO   AS ACCTNO,
         C_IBS_APPL_CODE AS ACCTCODE,
         U_IBS_R_APPL_NO AS CUSTNO,
-        C_IBS_E1_TO_E2  AS RLENCODE,
-        C_IBS_E2_TO_E1  AS PRISEC
+        try_cast(C_IBS_E1_TO_E2 AS INTEGER) AS RLENCODE,
+        try_cast(C_IBS_E2_TO_E1 AS INTEGER) AS PRISEC
     FROM rlen_files
     WHERE PRISEC = 901
       AND RLENCODE IN (3,11,12,13,14,16,17,18,19,21,22,23,27,28)
@@ -55,12 +50,12 @@ con.execute(f"""
 con.execute(f"""
     CREATE OR REPLACE TABLE loan AS
     SELECT 
-        substr(ACCTNO,1,11) AS ACCTNO,
-        substr(IND,1,3) AS STAT,
+        ACCTNO,
+        IND AS STAT,
         CASE 
-            WHEN trim(IND) = 'B' THEN 20
-            WHEN trim(IND) = 'G' THEN 17
-            WHEN trim(IND) = 'B/G' THEN 28
+            WHEN IND = 'B' THEN 20
+            WHEN IND = 'G' THEN 17
+            WHEN IND = 'B/G' THEN 28
             ELSE NULL
         END AS CODE
     FROM borwguar_unq
@@ -80,7 +75,8 @@ con.execute("""
         l.CODE,
         l.STAT
     FROM loan l
-    JOIN rlen r ON l.ACCTNO = r.ACCTNO
+    JOIN rlen r 
+    ON l.ACCTNO = r.ACCTNO
     WHERE NOT (
         l.CODE = r.RLENCODE
         OR (r.RLENCODE = 21 AND l.CODE = 17)
@@ -98,8 +94,11 @@ outfile_table = """
         LPAD(CAST(RLENCODE AS VARCHAR),3,'0') AS RLENCODE,
         LPAD(CAST(CODE AS VARCHAR),3,'0') AS CODE,
         STAT
+        ,{year} AS year
+        ,{month} AS month 
+        ,{day} AS day
     FROM merge1
-"""
+""".format(year=year,month=month,day=day)
 
 # ============================================================
 # STEP 6: OUTPUT FOR UPDATE (CIUPDRLN)
@@ -113,10 +112,42 @@ updf_table = """
         CUSTNO,
         '901' AS CONST3,
         LPAD(CAST(CODE AS VARCHAR),3,'0') AS CODE
+        ,{year} AS year
+        ,{month} AS month 
+        ,{day} AS day
     FROM merge1
-"""
+""".format(year=year,month=month,day=day)
 
 # ============================================================
 # COMPLETION LOG
 # ============================================================
-print("✅ CCRSRLEB Python Conversion Completed")
+out = """
+    SELECT
+        *
+        ,{year} AS year
+        ,{month} AS month 
+        ,{day} AS day
+    FROM borwguar_unq
+""".format(year=year,month=month,day=day)
+
+queries = {
+    "LIABFILE_BORWGUAR_UNQ"                 : out,
+    "CIS_UPDRLEN_BORWGTOR"             : outfile_table,
+    "CIS_UPDRLEN_UPDATE"             : updf_table
+}
+
+for name, query in queries.items():
+    parquet_path = parquet_output_path(name)
+    csv_path = csv_output_path(name)
+
+    con.execute(f"""
+    COPY ({query})
+    TO '{parquet_path}'
+    (FORMAT PARQUET, PARTITION_BY (year, month, day), OVERWRITE_OR_IGNORE true);  
+     """)
+    
+    con.execute(f"""
+    COPY ({query})
+    TO '{csv_path}'
+    (FORMAT CSV, HEADER, DELIMITER ',', OVERWRITE_OR_IGNORE true);  
+     """)
