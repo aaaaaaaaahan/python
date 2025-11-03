4c6d186e6a6d40f3891c3301cf7a6b5dc1f495c4
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-11-03 18:41:42 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,7 +1,1 @@
-from pathlib import Path
-
-output_folder = Path(csv_output_path())  # note the parentheses!
-output_folder.mkdir(parents=True, exist_ok=True)
-
-for table, filename in outputs.items():
-    write_fixed_width_txt(table, output_folder / filename, title=table)
+TypeError: csv_output_path() missing 1 required positional argument: 'name'
