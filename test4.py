5b8809296bff21cceb996713d34241bc739e7e21
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-11-03 18:40:51 +0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,3 +1,7 @@
-File "/usr/lib64/python3.9/posixpath.py", line 76, in join
-    a = os.fspath(a)
-TypeError: expected str, bytes or os.PathLike object, not function
+from pathlib import Path
+
+output_folder = Path(csv_output_path())  # note the parentheses!
+output_folder.mkdir(parents=True, exist_ok=True)
+
+for table, filename in outputs.items():
+    write_fixed_width_txt(table, output_folder / filename, title=table)
