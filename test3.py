a7ddf80ba76d52b7f62f2b568e7130d15018a276
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-11-12 17:20:12 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -52,8 +52,6 @@ out = """
     FROM MERGE
 """.format(year=year,month=month,day=day)
 
-df = con.execute(out).fetchdf()
-
 queries = {
     "CIS_LONGNAME_NONE"                      : out
 }
@@ -77,18 +75,22 @@ for name, query in queries.items():
 # ---------------------------------------------------------------------
 # Output as TXT
 # ---------------------------------------------------------------------
-txt_path = csv_output_path(f"CIS_DJW_DPACCT_{report_date}").replace(".csv", ".txt")
+txt_queries = {
+    "CIS_LONGNAME_NONE"                      : out
+}
 
-res = con.execute(out)
-columns = [desc[0] for desc in res.description]
-rows = res.fetchall()
+for txt_name, txt_query in txt_queries.items():
+    txt_path = csv_output_path(f"{txt_name}_{report_date}").replace(".csv", ".txt")
 
-with open(txt_path, "w", encoding="utf-8") as f:
-    for _, row in df.iterrows():
-        line = (
-            f"{str(row['CUSTNO']).ljust(20)}"
-            f"{str(row['ACCTCODE']).ljust(5)}"
-            f"{str(row['ACCTNOX']).ljust(20)}"
-            f"{str(row['OPENDX']).ljust(10)}"
-        )
-        f.write(line + "\n")
+    res = con.execute(txt_query)
+    df_txt = res.fetchdf()
+
+    with open(txt_path, "w", encoding="utf-8") as f:
+        for _, row in df_txt.iterrows():
+            line = (
+                f"{str(row['CUSTNO']).ljust(20)}"
+                f"{str(row['ACCTCODE']).ljust(5)}"
+                f"{str(row['ACCTNOX']).ljust(20)}"
+                f"{str(row['OPENDX']).ljust(10)}"
+            )
+            f.write(line + "\n")
