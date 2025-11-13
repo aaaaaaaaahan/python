36e63926c7c65ada870a6e89698ea9f06d7ac95b
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-11-13 18:48:40 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -5,112 +5,103 @@ import datetime
 batch_date = (datetime.date.today() - datetime.timedelta(days=1))
 year, month, day = batch_date.year, batch_date.month, batch_date.day
 report_date = batch_date.strftime("%d-%m-%Y")
+DATE1 = batch_date.strftime('%Y%m%d')
+DATE2 = batch_date.strftime('%Y%m%d')
 
-# ---------------------------------------------------------------------
-# DUCKDB PROCESSING
-# ---------------------------------------------------------------------
+# -------------------------------
+# CONNECT TO DUCKDB
+# -------------------------------
 con = duckdb.connect()
+cis = get_hive_parquet('CIS_CUST_DAILY')
 
-# Load input parquet into DuckDB
+# -------------------------------
+# LOAD CONSENT DATA
+# -------------------------------
+# Convert EFFTIMESTAMP to inverted value like in SAS: 100000000000000 - EFFTIMESTAMP
 con.execute(f"""
-    CREATE OR REPLACE TABLE CISEOD AS
-    SELECT *
-    FROM '{host_parquet_path("CIDARPGS.parquet")}'
+    CREATE OR REPLACE VIEW consent AS
+    SELECT
+        CUSTNO,
+        100000000000000 - CAST(EFFTIMESTAMP AS BIGINT) AS EFFDATETIME,
+        CAST(100000000000000 - CAST(EFFTIMESTAMP AS BIGINT) AS VARCHAR) AS EFFDATETIMEX,
+        SUBSTR(CAST(100000000000000 - CAST(EFFTIMESTAMP AS BIGINT) AS VARCHAR), 1, 8) AS EFFDATE,
+        SUBSTR(CAST(100000000000000 - CAST(EFFTIMESTAMP AS BIGINT) AS VARCHAR), 9, 6) AS EFFTIME,
+        KEYWORD,
+        CHANNEL,
+        CONSENT
+    FROM '{host_parquet_path("REMARKS_CONSENT_ALL.parquet")}'
 """)
 
-# ---------------------------------------------------------------------
-# Transform logic (convert SAS logic into SQL)
-# ---------------------------------------------------------------------
-# This will handle:
-#  - Filtering REPORTNO=8106 and SORTSETNO=1
-#  - Deriving CONSENTX from MISC(A–J)
-#  - Mapping 001→Y, 002→N
-#  - Formatting date from UPDDATE (PD6 numeric to YYYY-MM-DD)
-#  - Add fixed constant fields
-# ---------------------------------------------------------------------
+# -------------------------------
+# LOAD CUSTOMER DATA
+# -------------------------------
+# Filter out specific ACCTCODE, invalid ACCTNOC, and empty ALIASKEY & TAXID
+con.execute(f"""
+    CREATE OR REPLACE VIEW cust AS
+    SELECT DISTINCT *
+    FROM read_parquet('{cis[0]}')
+    WHERE ACCTCODE NOT IN ('DP   ','LN   ','EQC  ','FSF  ')
+      AND ACCTNOC > '1000000000000000'
+      AND ACCTNOC < '9999999999999999'
+      AND NOT (ALIASKEY = '' AND TAXID = '')
+""")
+
+# -------------------------------
+# MERGE CONSENT AND CUSTOMER DATA
+# -------------------------------
 con.execute("""
-    CREATE OR REPLACE TABLE CONSENT AS
-    WITH BASE AS (
-        SELECT 
-            BANKNO,
-            REPORTNO,
-            SORTSETNO,
-            UPDATEOPERATOR,
-            UPDDATE,
-            CUSTNO,
-            CASE 
-                WHEN MISCA IN ('07A','07C','07D') THEN CONSENTA
-                WHEN MISCB IN ('07A','07C','07D') THEN CONSENTB
-                WHEN MISCC IN ('07A','07C','07D') THEN CONSENTC
-                WHEN MISCD IN ('07A','07C','07D') THEN CONSENTD
-                WHEN MISCE IN ('07A','07C','07D') THEN CONSENTE
-                WHEN MISCF IN ('07A','07C','07D') THEN CONSENTF
-                WHEN MISCG IN ('07A','07C','07D') THEN CONSENTG
-                WHEN MISCH IN ('07A','07C','07D') THEN CONSENTH
-                WHEN MISCI IN ('07A','07C','07D') THEN CONSENTI
-                WHEN MISCJ IN ('07A','07C','07D') THEN CONSENTJ
-                ELSE ''
-            END AS CONSENTX
-        FROM CISEOD
-        WHERE REPORTNO = 8106 AND SORTSETNO = 1
-    ),
-    FILTERED AS (
-        SELECT *
-        FROM BASE
-        WHERE CONSENTX <> ''
-    ),
-    FINAL AS (
-        SELECT DISTINCT
-            BANKNO,
-            CUSTNO AS CUSTNOX,
-            'CUST' AS APPLCODE,
-            'BATCH' AS UPDATESOURCE,
-            CASE 
-                WHEN CONSENTX = '001' THEN 'Y'
-                WHEN CONSENTX = '002' THEN 'N'
-                ELSE ''
-            END AS CONSENT,
-            -- Convert numeric date to YYYY-MM-DD
-            CASE 
-                WHEN length(cast(UPDDATE AS VARCHAR)) = 6 THEN 
-                    substr(lpad(cast(UPDDATE AS VARCHAR),8,'0'),5,4) || '-' ||
-                    substr(lpad(cast(UPDDATE AS VARCHAR),8,'0'),3,2) || '-' ||
-                    substr(lpad(cast(UPDDATE AS VARCHAR),8,'0'),1,2)
-                ELSE ''
-            END AS UPDATEDATE,
-            '00000000' AS UPDATETIME,
-            UPDATEOPERATOR
-        FROM FILTERED
-    )
-    SELECT * FROM FINAL
+    CREATE OR REPLACE VIEW merge_data AS
+    SELECT c.*, co.CONSENT, co.CHANNEL, co.EFFDATETIME, co.EFFDATE, co.EFFTIME
+    FROM cust c
+    INNER JOIN consent co
+    ON c.CUSTNO = co.CUSTNO
+""")
+
+# -------------------------------
+# ALLFILE
+# -------------------------------
+con.execute("""
+    CREATE OR REPLACE TABLE all AS
+    SELECT ACCTNOC, ALIASKEY, ALIAS, TAXID, CONSENT, CHANNEL
+    FROM merge_data
+    ORDER BY ACCTNOC
+""")
+
+# -------------------------------
+# DAILY FILE (DAY)
+# -------------------------------
+con.execute(f"""
+    CREATE OR REPLACE TABLE daily AS
+    SELECT ACCTNOC, ALIASKEY, ALIAS, TAXID, CONSENT, CHANNEL
+    FROM merge_data
+    WHERE EFFDATE = '{DATE2}' AND CHANNEL != 'UNIBATCH'
+    ORDER BY ACCTNOC
 """)
 
 # ---------------------------------------------------------------------
 # OUTPUT PARQUET
 # ---------------------------------------------------------------------
-out = """
+out1 = """
     SELECT 
-        BANKNO,
-        'CUST' AS INDICATOR1,
-        CUSTNOX,
-        CONSENT,
-        UPDATEDATE,
-        'X' AS INDICATOR2,
-        UPDATESOURCE,
-        UPDATEDATE,
-        UPDATETIME,
-        UPDATESOURCE,
-        UPDATEDATE,
-        UPDATETIME,
-        UPDATEOPERATOR,
+        *,
         {year} AS year,
         {month} AS month,
         {day} AS day
-    FROM CONSENT
+    FROM all
+""".format(year=year,month=month,day=day)
+
+out2 = """
+    SELECT 
+        *,
+        {year} AS year,
+        {month} AS month,
+        {day} AS day
+    FROM daily
 """.format(year=year,month=month,day=day)
 
 queries = {
-    "CIDARPGS_CONSENT"                      : out
+    "UNICARD_MAILFLAG_ALL"                      : out1,
+    "UNICARD_MAILFLAG_DLY"                      : out2
 }
 
 for name, query in queries.items():
@@ -133,7 +124,8 @@ for name, query in queries.items():
 # OUTPUT FIXED-WIDTH TEXT
 # ---------------------------------------------------------------------
 txt_queries = {
-        "CIDARPGS_CONSENT"                      : out
+    "UNICARD_MAILFLAG_ALL"                      : out1,
+    "UNICARD_MAILFLAG_DLY"                      : out2
 }
 
 for txt_name, txt_query in txt_queries.items():
@@ -143,17 +135,11 @@ for txt_name, txt_query in txt_queries.items():
     with open(txt_path, "w", encoding="utf-8") as f:
         for _, row in df_txt.iterrows():
             line = (
-                f"{str(row['BANKNO']).ljust(3)}"
-                f"{str(row['INDICATOR1']).ljust(5)}"
-                f"{str(row['CUSTNOX']).ljust(11)}"
+                f"{str(row['ACCTNOC']).ljust(16)}"
+                f"{str(row['ALIASKEY']).ljust(3)}"
+                f"{str(row['ALIAS']).ljust(12)}"
+                f"{str(row['TAXID']).ljust(12)}"
                 f"{str(row['CONSENT']).ljust(1)}"
-                f"{str(row['UPDATEDATE']).ljust(10)}"
-                f"{str(row['INDICATOR2']).ljust(1)}"
-                f"{str(row['UPDATESOURCE']).ljust(5)}"
-                f"{str(row['UPDATEDATE']).ljust(10)}"
-                f"{str(row['UPDATETIME']).ljust(8)}"
-                f"{str(row['UPDATESOURCE']).ljust(5)}"
-                f"{str(row['UPDATEDATE']).ljust(10)}"
-                f"{str(row['UPDATETIME']).ljust(8)}"
+                f"{str(row['CHANNEL']).ljust(8)}"
             )
             f.write(line + "\n")
