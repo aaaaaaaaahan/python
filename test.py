d37990c9d1143fd60d6c63187c532d4135d8d25e
aaaaaaaaahan <133189571+aaaaaaaaahan@users.noreply.github.com>
2025-11-21 10:58:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,137 +2,128 @@ convert program to python with duckdb and pyarrow
 duckdb for process input file and output parquet&txt
 assumed all the input file ady convert to parquet can directly use it
 
-//CIPHB4AF JOB MSGCLASS=X,MSGLEVEL=(1,1),REGION=6M,NOTIFY=&SYSUID       JOB67804
-//*---------------------------------------------------------------------
-//FILEB4AF EXEC SAS609
+//CIBRRPTB JOB BR-FILE,'BRANCH-INFO-RPT',MSGCLASS=A,MSGLEVEL=(1,1),     JOB14350
+//         USER=OPCC,
+//         REGION=64M,NOTIFY=&SYSUID,CLASS=A
+//*
+/*JOBPARM S=S1M2
+//*
+//**********************************************************************
+//*         E-BANKING - PRINT BRANCH GENERAL INFO REPORT (PBB)
+//**********************************************************************
+//*
+//*********************************************************************
+//* DELETE OUTPUT FILES
+//*********************************************************************
+//*DELETE   EXEC PGM=IEFBR14,COND=(0,NE)
+//*DEL1     DD DSN=EBANK.BRANCH.OFFICER.COMBINE.RPT,
+//*         DISP=(MOD,DELETE,DELETE),
+//*         SPACE=(CYL,(100,200),RLSE)
+//**********************************************************************
+//SAS609   EXEC SAS609,REGION=0M,WORK='50000,50000'
+//CONFIG    DD DISP=SHR,DSN=SYS3.SAS.V609.CNTL(BATCHXA)
+//STEPLIB   DD DISP=(SHR,PASS),DSN=&LOAD
+//          DD DISP=SHR,DSN=SYS3.SAS.V609.LIBRARY
 //IEFRDER   DD DUMMY
-//SORTWK01  DD UNIT=SYSDA,SPACE=(CYL,(1000,500))
-//SORTWK02  DD UNIT=SYSDA,SPACE=(CYL,(1000,500))
-//SORTWK03  DD UNIT=SYSDA,SPACE=(CYL,(1000,500))
-//SORTWK04  DD UNIT=SYSDA,SPACE=(CYL,(1000,500))
-//CTRLDATE  DD DISP=SHR,DSN=SRSCTRL1(0)
-//CIPHONET  DD DISP=SHR,DSN=UNLOAD.CIPHONET.FB
-//CISFILE   DD DISP=SHR,DSN=CIS.CUST.DAILY
-//DPTRBALS  DD DISP=SHR,DSN=DPTRBLGS
-//OUTFILE   DD DSN=CIPHONET.ATM.CONTACT(+1),
-//             DISP=(NEW,CATLG,DELETE),
-//             UNIT=SYSDA,SPACE=(CYL,(100,50),RLSE),
-//             DCB=(LRECL=200,BLKSIZE=0,RECFM=FB)
+//INSFILE   DD DSN=EBANK.BRANCH.OFFICER.COMBINE,
+//          DISP=SHR
+//*OUTRPT    DD DSN=EBANK.BRANCH.OFFICER.COMBINE.RPT,
+//*          DISP=(NEW,CATLG,DELETE),
+//*          UNIT=(SYSDA,5),
+//*          SPACE=(CYL,(100,50),RLSE),
+//*          DCB=(LRECL=133,BLKSIZE=0,RECFM=FB)
+//DFSVSAMP  DD DSN=RBP2.IB330P.CONTROL(IBAMS#00),DISP=SHR
 //SASLIST   DD SYSOUT=X
 //SYSIN     DD *
-OPTIONS IMSDEBUG=N YEARCUTOFF=1950 SORTDEV=3390 ERRORS=5;
-OPTIONS NODATE NONUMBER NOCENTER;
+OPTIONS IMSDEBUG=N YEARCUTOFF=1950 SORTDEV=3390 ERRORS=0;
+OPTIONS NODATE NONUMBER;
 TITLE;
- /*----------------------------------------------------------------*/
- /*    DATA DECLARATION                                            */
- /*----------------------------------------------------------------*/
-   DATA SRSDATE;
-      INFILE CTRLDATE;
-        INPUT @001  SRSYY    4.
-              @005  SRSMM    2.
-              @007  SRSDD    2.;
-
-      CURDT = PUT(SRSYY,Z4.) || PUT(SRSMM,Z2.) || PUT(SRSDD,Z2.);
-      CALL SYMPUT('CURDT', PUT(CURDT,8.));
-      CALL SYMPUT('DAY', PUT(SRSDD,Z2.));
-      CALL SYMPUT('MTH', PUT(SRSMM,Z2.));
-      CALL SYMPUT('YEAR', PUT(SRSYY,4.));
-   RUN;
-PROC PRINT DATA=SRSDATE(OBS=10);
-           TITLE 'DATE FORMAT ** MMDDYYYY **   ';RUN;
-
- DATA PHONE;
-    INFILE CIPHONET;
-            INPUT @001  BANKNO             $3.
-                  @004  APPLCODE           $5.
-                  @009  CUSTNO             $11.
-                  @029  PHONETYPE          $15.
-                  @044  PHONEPAC           PD8.
-                  @052  PHONEPREV          PD8.
-                  @060  INDORG             $1.
-                  @061  FIRSTDATE          $10.
-                  @071  PROMPTNO           PD1.
-                  @072  PROMTSOURCE        $5.
-                  @077  PROMPTDATE         $10.
-                  @087  PROMPTTIME         $8.
-                  @095  UPDSOURCE          $5.
-                  @100  UPDDATE            $10.
-                  @100  UPDYY               4.
-                  @105  UPDMM               2.
-                  @108  UPDDD               2.
-                  @110  UPDTIME            $8.
-                  @118  UPDOPER            $8.
-                  @126  TRXAPPL            $5.
-                  @131  TRXACCTNO          $20.
-                  @131  TRXACCTDP           11.
-                  @151  PHONENEW           PD8.; /* ESMR 2011-3700*/
-      IF UPDSOURCE = 'INIT' THEN DELETE;
-      RECDT = PUT(UPDYY,Z4.) || PUT(UPDMM,Z2.) || PUT(UPDDD,Z2.);
-      IF RECDT = &CURDT THEN OUTPUT;
- RUN;
- PROC PRINT DATA=PHONE(OBS=5);TITLE 'PHONE';RUN;
- PROC SORT  DATA=PHONE; BY CUSTNO ; RUN;
-
- DATA CIS;
-    SET CISFILE.CUSTDLY;
-        KEEP CUSTNO CUSTNAME SECPHONE ALIASKEY ALIAS ;
-        RUN;
- PROC SORT  DATA=CIS NODUPKEY; BY CUSTNO ; RUN;
-
-DATA MRG1;
-   MERGE PHONE(IN=A) CIS(IN=B) ; BY CUSTNO;
-   IF A THEN OUTPUT;
-   RUN;
-PROC PRINT DATA=MRG1(OBS=5);TITLE 'MERGE1';RUN;
-PROC SORT  DATA=MRG1; BY TRXACCTDP ;RUN;
-
-  DATA DEPOSIT;
-     INFILE DPTRBALS MISSOVER;
-     INPUT @24  REPTNO       PD3.
-           @27  FMTCODE      PD2. @;
-
-     IF (REPTNO = 1001 AND (FMTCODE IN (1,10,22))) THEN DO;
-        INPUT @106   ACCTBRCH    PD4.
-              @110   TRXACCTDP   PD6.
-              @164   OPENDATE     PD6.
-              @716   OPENIND     $1.;   /* VALID VALUE = SPACE,B,C,P,Z*/
-               IF TRXACCTDP NE '';
-               IF OPENIND EQ '';
-     END;
-  RUN;
- PROC SORT DATA=DEPOSIT ; BY TRXACCTDP; RUN;
- PROC PRINT DATA=DEPOSIT(OBS=5);TITLE 'DEPOSIT';RUN;
-
-DATA MRG2;
-   MERGE MRG1(IN=A) DEPOSIT(IN=B) ; BY TRXACCTDP;
-   IF A THEN OUTPUT;
-   RUN;
-PROC SORT  DATA=MRG2; BY CUSTNO ;RUN;
-PROC PRINT DATA=MRG2(OBS=5);TITLE 'MERGE2';RUN;
 
  /*----------------------------------------------------------------*/
- /*   OUTPUT DETAIL REPORT                                         */
+ /*          PRINT 'BRANCH GENERAL INFO REPORT' (PBB)              */
  /*----------------------------------------------------------------*/
-DATA TEMPOUT;
-  SET MRG2;
-  FILE OUTFILE;
-     IF UPDSOURCE NOT IN ('ATM','EBK') THEN UPDSOURCE = 'OTC';
-     PUT @001  '033'
-         @004  TRXAPPL           $5.
-         @009  TRXACCTDP         10.
-         @029  PHONENEW          Z11.      /* CIPHONET  (AF UPDATE)*/
-         @040  PHONEPREV         Z11.      /* ACCT LEVEL(B4 UPDATE)*/
-         @051  SECPHONE          Z11.      /* CIS LEVEL (NO CHANGE)*/
-         @062  ACCTBRCH          Z7.
-         @069  CUSTNO            $20.
-         @089  CUSTNAME          $40.
-         @129  ALIASKEY          $03.
-         @132  ALIAS             $37.
-         @169  UPDDD             Z2.
-         @171  '/'
-         @172  UPDMM             Z2.
-         @174  '/'
-         @175  UPDYY             Z4.
-         @179  UPDSOURCE         $05.;
+
+DATA GETDATE;
+     DT=TODAY();
+     DD=PUT(DAY(DT),Z2.);
+     MM=PUT(MONTH(DT),Z2.);
+     CCYY=PUT(YEAR(DT),Z4.);
+     YY = SUBSTR(PUT(CCYY, 4.),3,2);
+     CALL SYMPUT('DAY', PUT(DD,2.));
+     CALL SYMPUT('MONTH', PUT(MM,2.));
+     CALL SYMPUT('YEAR', PUT(YY,2.));
+RUN;
+
+DATA BRFILE;
+   INFILE INSFILE;
+   INPUT @1    BANKNBR            $1.
+         @2    BRNBR              7.
+         @9    BRABBRV            $3.
+         @12   BRNAME             $20.
+         @32   BRADDRL1           $35.
+         @67   BRADDRL2           $35.
+         @102  BRADDRL3           $35.
+         @137  BRPHONE            $11.
+         @148  BRSTCODE           3.
+         @150  BRRPS              $4;
+RUN;
+
+DATA _NULL_;
+  SET BRFILE END=EOF;
+  FILE PRINT HEADER=NEWPAGE NOTITLE;
+  LINECNT = 0;
+  FORMAT BANKNAME $45.;
+
+  IF LINECNT >= 52 THEN
+     PUT _PAGE_;
+
+     PUT @1    BRNBR             7.
+         @12   BRABBRV           $3.
+         @20   BRNAME            $20.
+         @45   BRADDRL1          $35.
+         @88   BRPHONE           $11.
+         @105  BRSTCODE          3.
+        /@45   BRADDRL2          $35.
+        /@45   BRADDRL3          $35.///;
+
+    LINECNT + 6;
+    BRCNT + 1;
+
+  IF EOF THEN DO;
+    PUT @1   'TOTAL NUMBER OF BRANCH = ' BRCNT    4.;
+  END;
   RETURN;
-  RUN;
+
+  NEWPAGE :
+    PAGECNT+1;
+    LINECNT = 0;
+
+    IF BANKNBR = 'B' THEN
+       BANKNAME = 'PUBLIC BANK BERHAD';
+    ELSE IF BANKNBR = 'F' THEN
+            BANKNAME = 'PUBLIC FINANCE BERHAD';
+
+    PUT @1   'REPORT ID   : BNKCTL/BR/FILE/RPTS'
+        @55   BANKNAME
+        @94  'PAGE        : ' PAGECNT   4.
+       /@1   'PROGRAM ID  : CIBRRPTB'
+        @94  'REPORT DATE : ' "&DAY" '/' "&MONTH" '/' "&YEAR"
+       /@52  'BRANCH GENERAL INFO REPORT'
+       /@52  '==========================';
+
+    PUT ///@2   'BR NBR'
+        @12  'ABBRV'
+        @20  'NAME'
+        @45  'ADDRESS'
+        @88  'PHONE'
+        @106 'STATE CODE';
+    PUT @2   '------'
+        @12  '-----'
+        @20  '----'
+        @45  '-------'
+        @88  '-----'
+        @106 '----------';
+
+    LINECNT = 8;
+  RETURN;
+RUN;
